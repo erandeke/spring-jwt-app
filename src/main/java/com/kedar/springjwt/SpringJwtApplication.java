02b51fadc5d14d5a903e484316b2a844d16e68d1
Kedar Erande <erande.kedar@gmail.com>
2022-09-23 21:49:15 +0530

    spring-jwt

---
+++ src/main/java/com/kedar/springjwt/SpringJwtApplication.java
@@ -0,0 +1,16 @@
+package com.kedar.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("test");
+        System.out.println("test");
+        SpringApplication.run(SpringJwtApplication.class, args);
+    }
+
+}
